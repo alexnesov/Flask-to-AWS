d17b2b850b337630e9cd3495240791280464257f
alexnesov <alexandrenesovic@gmail.com>
2020-10-18 16:24:17 +0200

    imports correction

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+
+# edit the URI below to add your RDS password and your AWS URL
+# The other elements are the same as used in the tutorial
+# format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
+
+db_user = os.environ.get('aws_db_user')
+db_pass = os.environ.get('aws_db_pass')
+db_endpoint = os.environ.get('aws_db_endpoint')
+
+
+SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{db_user}:{db_pass}@{db_endpoint}/flask'
+
+# Uncomment the line below if you want to work with a local DB
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+
+SQLALCHEMY_POOL_RECYCLE = 3600
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
