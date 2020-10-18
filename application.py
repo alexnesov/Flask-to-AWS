575340dea66931f762cab5bb11277d2a421b4511
alexnesov <alexandrenesovic@gmail.com>
2020-10-18 15:02:26 +0200

    new

--- application.py
+++ application.py
@@ -2,7 +2,6 @@
 '''
 Simple Flask application to test deployment to Amazon Web Services
 Uses Elastic Beanstalk and RDS
-Author: Scott Rodkey - rodkeyscott@gmail.com
 Step-by-step tutorial: https://medium.com/@rodkey/deploying-a-flask-application-on-aws-a72daba6bb80
 '''
 
